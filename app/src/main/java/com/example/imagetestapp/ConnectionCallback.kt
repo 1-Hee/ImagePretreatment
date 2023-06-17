d641a5b279380e9df1b6c9f27ef5e60858608b42
1-Hee <onehee9710@gmail.com>
2023-06-17 17:58:17 +0900

    미리보기 화면을 만들 때, 현재 화면의 사이즈와 화면 방향을 전달할 콜백 인스턴스

--- app/src/main/java/com/example/imagetestapp/ConnectionCallback.kt
+++ app/src/main/java/com/example/imagetestapp/ConnectionCallback.kt
@@ -1,4 +1,8 @@
 package com.example.imagetestapp
 
+import android.util.Size
+
+// 우리가 미리보기 화면을 만들 때, 현재 화면의 사이즈와 화면 방향을 전달해주는 용도
 interface ConnectionCallback {
+    fun onPreviewSizeChosen(size: Size, cameraRotation: Int)
 }
